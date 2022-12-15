9d292c7fd478d5394fd3a290960a4961349c9ef2
Yuhaigiub <buigiahuynt@gmail.com>
2022-12-16 01:56:33 +0700

    polising

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ let currentHighlight = 0;
 			.getComputedStyle(images[currentHighlight])
 			.getPropertyValue("border-color")}`
 	);
-	Array.from(images).map((image, index) => {
+	Array.from(images).map((image, index, arr) => {
 		image.style.setProperty("background-image", `url('./images1/image${index + 1}.jpg')`);
 		image.addEventListener("mouseover", (e) => {
+			document.documentElement.style.setProperty("--small-color", "red");
+			document.documentElement.style.setProperty("--large-color", "teal");
+
+			arr.map((img) => {
+				img.style.setProperty("animation-play-state", "paused");
+			});
+
 			images[currentHighlight].style.setProperty("filter", "grayscale(1)");
 			currentHighlight = index;
 			images[index].style.setProperty("filter", "grayscale(0)");
@@ -30,6 +37,12 @@ let currentHighlight = 0;
 			middle.style.setProperty("background-image", `url('./images1/image${index + 1}.jpg')`);
 		});
 		image.addEventListener("mouseout", (e) => {
+			document.documentElement.style.setProperty("--small-color", "chartreuse");
+			document.documentElement.style.setProperty("--large-color", "white");
+
+			arr.map((img) => {
+				img.style.setProperty("animation-play-state", "running");
+			});
 			icon.style.setProperty("animation-play-state", "running");
 		});
 	});
